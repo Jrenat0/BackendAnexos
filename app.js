8b1422d2a6ceeab76ffe919c4f92ac8b9cd400da
Joaquín Martínez Olivares <132187041+Jrenat0@users.noreply.github.com>
2026-03-04 15:35:46 -0300

    Set cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from 'dotenv';
+import cors from 'cors';
 import {connectDB} from './config/db.js';
 import anexoRoutes from './routes/anexos.routes.js';
 import listEndpoints from 'express-list-endpoints';
@@ -7,6 +8,7 @@ import listEndpoints from 'express-list-endpoints';
 dotenv.config();
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 const PORT = process.env.PORT;
 
